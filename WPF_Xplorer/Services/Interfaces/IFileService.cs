460b1cb6d24a49a41815c177ed9cc6bfe9b3617c
skafa <skafa@SKAFA>
2021-12-06 18:56:58 +0200

    Add project files.

---
+++ WPF_Xplorer/Services/Interfaces/IFileService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Xplorer.Services.Interfaces
+{
+    public interface IFileService
+    {
+        List<PDF> Open(string fileName); // пдф файл который нужно открыть
+    }
+}
---
+++ WPF_Xplorer/ViewModels/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace WPF_Xplorer.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
